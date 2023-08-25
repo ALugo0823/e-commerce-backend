2e54614cbe2b82101dfc3468d76f28c3710489f6
lugo.kse23@gmail.com <lugo.kse23@gmail.com>
2023-08-24 17:08:45 -0700

    models complete

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // here we define the columns of the Category model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
   },
   {
     sequelize,
